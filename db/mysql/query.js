de7d67ff2350b4e482cf84ec7bc0bfe263cad74d
Leonardo Theoson <thefatflyer@gmail.com>
2019-07-09 10:26:47 +0200

    merged mysql sample DB REST into weatherapp mongo REST

---
+++ db/mysql/query.js
@@ -0,0 +1,201 @@
+import db from './db'
+
+
+export const getEmployees = (conn, limit)=>{
+	return new Promise((resolve, reject)=>{
+		let query = `SELECT  de.emp_no, e.first_name, e.last_name, e.hire_date, t.title, d.dept_name FROM employees e INNER JOIN titles t ON e.emp_no = t.emp_no INNER JOIN dept_emp de ON e.emp_no = de.emp_no INNER JOIN departments d ON de.dept_no = d.dept_no WHERE t.to_date = '9999-01-01' AND de.to_date = '9999-01-01' `
+
+		if (typeof limit !== "undefined"){
+			query += "LIMIT "+limit
+		}
+
+		//console.log(query)
+		conn.query(query, (err, res, fields)=>{
+			//console.log("---")
+			if (!err) {
+				let data = []
+				for (let i = 0; i < res.length; i++) {
+					data.push({
+						emp_no: res[i].emp_no,
+						first_name: res[i].first_name,
+						last_name: res[i].last_name,
+						title: res[i].title,
+						hire_date: res[i].hire_date,
+						dept_name: res[i].dept_name
+					})
+				}
+				resolve(data)
+			}else{
+				reject(err)
+			}
+		})
+	})
+}
+
+export const insertEmployee = (conn, emp_no, first_name, last_name, birth_date, gender, title, hire_date, dept_name)=>{
+	return new Promise(async(resolve, reject) => {
+		const dept_no = await insertDepartmentIfNotExist(conn, dept_name)
+
+		const query = "BEGIN; " +
+			`INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date) VALUES(${emp_no}, "${birth_date}", "${first_name}", "${last_name}", "${gender}", "${hire_date}"); ` +
+			`INSERT INTO dept_emp(emp_no, dept_no, from_date, to_date) VALUES(${emp_no}, "${dept_no}", "${hire_date}", "9999-01-01"); ` +
+			`INSERT INTO titles(emp_no, title, from_date, to_date) VALUES(${emp_no}, "${dept_no}", "${hire_date}", "9999-01-01"); ` +
+			"COMMIT;"
+
+		conn.query(query, (err, res)=>{
+			if (err){
+				console.error(err)
+				resolve(false)
+			} else{
+				resolve(true)
+			}
+		})
+	})
+}
+
+export const deleteEmployee = (conn, emp_no)=>{
+	return new Promise(resolve=>{
+		const query = "DELETE FROM employees WHERE emp_no = "+emp_no
+		conn.query(query, (err, res)=>{
+			if (err){
+				reject(err)
+			} else{
+				resolve(true)
+			}
+		})
+	})
+}
+
+export const updateEmployee = (conn, emp_no, first_name, last_name, birth_date, gender, title, hire_date, dept_name)=>{
+	return new Promise(async (resolve, reject)=>{
+		if (await isDepartmentChanged(conn, emp_no, dept_name)===true) {
+			const dept_no = await insertDepartmentIfNotExist(conn, dept_name)
+			Date.prototype.yyyymmdd = function() {
+				var mm = this.getMonth() + 1; // getMonth() is zero-based
+				var dd = this.getDate();
+
+				return [this.getFullYear(),
+					(mm>9 ? '' : '0') + mm,
+					(dd>9 ? '' : '0') + dd
+				].join('');
+			};
+
+			const dateNow = new Date().yyyymmdd();
+			const query = `UPDATE employees SET 
+					first_name = "${first_name}", 
+					last_name="${last_name}",
+					birth_date="${birth_date}",
+					gender="${gender}",
+					hire_date="${hire_date}" WHERE emp_no = ${emp_no};
+					UPDATE dept_emp SET 
+					dept_no='${dept_no}',
+					from_date='${dateNow}',
+					to_date='9999-01-01' WHERE emp_no = ${emp_no}`
+			console.log(query)
+			conn.query(query, (err, res)=>{
+				if (!err){
+					resolve(true)
+				} else{
+					reject(err)
+				}
+			})
+
+		}else{
+			const query = `UPDATE employees SET 
+					first_name = "${first_name}", 
+					last_name="${last_name}",
+					birth_date="${birth_date}",
+					gender="${gender}",
+					hire_date="${hire_date}" WHERE emp_no = ${emp_no};`
+			conn.query(query, (err, res)=>{
+				if (!err){
+					resolve(true)
+				} else{
+					reject(err)
+				}
+			})
+		}
+	})
+}
+
+const insertDepartmentIfNotExist = (conn, dept_name)=>{
+	return new Promise(async(resolve, reject)=>{
+		if (await checkIfDepartmentExist(conn, dept_name)){
+			conn.query(`SELECT dept_no FROM departments WHERE dept_name = "${dept_name}"`, (err, res)=>{
+				if (err){
+					reject(err)
+				}else {
+					resolve(res[0].dept_no)
+				}
+			})
+		}else{
+			const newId = await getNewDepartmentId(conn)
+			conn.query(`INSERT INTO departments (dept_no, dept_name) VALUES("${newId}" ,"${dept_name}")`, (err, res)=>{
+				if (err){
+					reject(err)
+				} else{
+					resolve(newId)
+				}
+			})
+		}
+	})
+}
+
+const checkIfDepartmentExist = (conn, dept_name)=>{
+	return new Promise((resolve, reject)=>{
+		conn.query("SELECT 'x' FROM departments WHERE dept_name = '"+dept_name+"'", (err, res)=>{
+			if (err){
+				reject(err)
+			} else{
+				if (res.length===0){
+					resolve(false)
+				} else{
+					resolve(true)
+				}
+			}
+		})
+	})
+}
+
+const getNewDepartmentId = (conn)=>{
+	return new Promise((resolve, reject)=>{
+
+		conn.query("SELECT MAX(dept_no) AS dept_no FROM departments", (err, res)=>{
+			if (err){
+				reject(err)
+			} else{
+				if (res.length===0){
+					resolve('d001')
+				} else {
+					let currId = res[0].dept_no
+					currId = currId.replace('d', '')
+					currId++
+					currId = new String(currId)
+					switch (currId.length){
+						case 1: currId = 'd00'+currId; break;
+						case 2: currId = 'd0'+currId; break;
+						default: currId = 'd0'+currId;
+					}
+					resolve(currId)
+				}
+			}
+		})
+	})
+}
+
+const isDepartmentChanged = (conn, emp_no, checkDeptName)=>{
+	return new Promise(resolve=>{
+		const query = `SELECT d.dept_name FROM departments d INNER JOIN dept_emp de ON d.dept_no = de.dept_no WHERE de.emp_no=${emp_no}`
+		conn.query(query, (err, res)=>{
+			if (!err){
+				const oldDept = res[0].dept_name
+				if (oldDept===checkDeptName){
+					console.log(oldDept+" with "+checkDeptName)
+					resolve(false)
+				} else{
+					resolve(true)
+				}
+			}
+		})
+	})
+}
--- routes/restApi.js
+++ routes/restApi.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { User } from '../db/mongodb/models/userModel';
-import { Tree } from '../db/mongodb/models/treeModel';
 import { City } from '../db/mongodb/models/cityModel'
+
+import {getEmployees, insertEmployee, updateEmployee, deleteEmployee} from "../db/mysql/query";
+import db from "../db/mysql/db";
 
 const router = express.Router();
 
@@ -123,332 +124,55 @@ router.route('/cities')
 		})
 	})
 
-// router.route('/users')
-//
-// 	// Gets all users
-// 	.get((req, res) => {
-// 		User.find((err, users) => {
-// 			if(err) {
-// 				res.send(err);
-// 			} else {
-// 				res.json(users);
-// 			}
-// 		})
-// 	})
-//
-// 	// Creates a user
-// 	.post((req, res) => {
-// 		/*
-// 			Example req.body
-// 			{ _id: 'sandiaga_uno' }
-// 		*/
-// 		const _id = req.body._id;
-// 		const data = new User({
-// 			_id : _id
-// 		});
-// 		data.save((err, user)=>{
-// 			if (err) {
-// 				res.json({
-// 					status: 'failed',
-// 					source: 'REST',
-// 					action: 'Add - User',
-// 					message: err
-// 				});
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Add - User',
-// 					message: user
-// 				});
-//
-//
-// router.route('/users/:userId')
-//
-// 	// Gets a specified user with their cities
-// 	.get((req, res) => {
-// 		User.findById(req.params.userId, (err, user) => {
-// 			if(err) res.send(err);
-//
-// 			res.json(user);
-// 		})
-// 	})
-//
-// 	// Creates a city for a specified user
-// 	.post( async(req, res) => {
-// 		/**
-// 		 *	Example req.body -> normal form
-// 		 *  	_id, cityName, isPublic
-// 		 */
-// 		const _id = req.params.userId
-// 		const city = {
-// 			id : req.body.id,
-// 			cityName: req.body.cityName,
-// 			isPublic: req.body.isPublic
-// 		}
-//
-// 		const user = await User.findById(_id)
-// 		let userCities = user.cities
-// 		let cityAlreadyExist = false
-// 		userCities.forEach((existingCity)=>{
-// 			if 	(existingCity.id===city.id)
-// 				cityAlreadyExist = true
-// 		})
-// 		if (!cityAlreadyExist) {
-// 			userCities.push(city)
-//
-// 			// User.update({_id: _id}, {
-// 			// 	$set: {
-// 			// 		cities: userCities
-// 			// 	}
-// 			// }, (err, response) => {
-// 			// 	const msg = err ? {status: 'failed', message: err} : {status: 'success', message: response}
-// 			// 	res.json(msg)
-// 			// })
-//
-// 			const id = _id;
-// 			const update = { $set: { cities: userCities } };
-// 			const options = { new: true };
-// 			User.findByIdAndUpdate(id, update, options, (err, doc) => {
-// 				if(err) {
-// 					res.json({
-// 						status: 'failed',
-// 						source: 'REST',
-// 						action: 'Add - User City',
-// 						message: err
-// 					});
-// 				} else {
-// 					res.json({
-// 						status: 'success',
-// 						source: 'REST',
-// 						action: 'Add - User City',
-// 						message: doc.get('cities').find(city => city.id === req.body.id)
-// 					});
-// 				}
-// 			});
-// 		}else{
-// 			res.json({
-// 				status: 'failed',
-// 				source: 'REST',
-// 				action: 'Add - User City',
-// 				message: 'City already exist'
-// 			})
-// 		}
-//
-// 		// User.updateOne(
-// 		// 	{ _id: _id },
-// 		// 	{ $addToSet: { cities: city } },
-// 		// 	(err, rawResponse) => {
-// 		// 		if(err) {
-// 		// 			res.send(err);
-// 		// 		} else {
-// 		// 			res.json(rawResponse);
-// 		// 		}
-// 		// 	})
-// 	})
-//
-// 	// Deletes a specified user
-// 	.delete((req, res) => {
-// 		User.findByIdAndDelete(req.params.userId, (err, user) => {
-// 			if(err) {
-// 				res.json({
-// 					status: 'failed',
-// 					source: 'REST',
-// 					action: 'Delete - User',
-// 					message: err
-// 				});
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Delete - User',
-// 					message: user
-// 				});
-// 			}
-// 		});
-// 	});
-//
-// router.route('/users/:userId/:cityId')
-//
-// 	// Edits a specified city for a specified user
-// 	.put((req, res) => {
-// 		/*
-// 			Example req.body
-// 			{
-// 				_id: 'foo,id',
-// 				cityName: 'foo',
-// 				isPublic: true
-// 			}
-// 		*/
-// 		const userId = req.params.userId;
-// 		const cityId = req.params.cityId;
-// 		const cityName = req.body.cityName;
-//
-// 		const query = { '_id': userId, 'cities.id': cityId };
-// 		const update = { $set: { 'cities.$.cityName': cityName } };
-// 		const options = { new: true };
-//
-// 		User.findOneAndUpdate(query, update, options, (err, doc) => {
-// 			if(err) {
-// 				res.json({
-// 					status: 'failed',
-// 					source: 'REST',
-// 					action: 'Edit - User City',
-// 					message: err
-// 				});
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Edit - User City',
-// 					message: doc.get('cities').find(city => city.id === cityId)
-// 				});
-// 			}
-// 		});
-// 	})
-//
-// 	// Deletes a specified city from a specified user
-// 	.delete((req, res) => {
-// 		User.updateOne(
-// 			{ _id: req.params.userId },
-// 			{ $pull: { 'cities': { 'id': req.params.cityId } } },
-// 			(err, rawResponse) => {
-// 				if(err) {
-// 					res.send(err);
-// 				} else {
-// 					res.json({
-// 						status: 'success',
-// 						source: 'REST',
-// 						action: 'Delete - User City',
-// 						message: rawResponse
-// 					});
-// 				}n
-// 			}
-// 		)
-// 	});
-//
-// router.route('/trees')
-//
-// 	// Gets all trees
-// 	.get((req, res) => {
-// 		Tree.find((err, trees) => {
-// 			if(err) {
-// 				res.send(err);
-// 			} else {
-// 				res.json(trees);
-// 			}
-// 		});
-// 	})
-//
-// 	// Creates a tree
-// 	.post((req, res) => {
-// 		/*
-// 			Example req.body
-// 			{
-// 				_id: 'apple tree',
-// 				description: 'an apple tree'
-// 			}
-// 		*/
-// 		const _id = req.body._id;
-// 		const description = req.body.description;
-// 		const data = new Tree({
-// 			_id : _id,
-// 			description: description
-// 		});
-// 		data.save((err, tree) => {
-// 			if (err) {
-// 				console.log(err);
-//
-// 				// Duplicate key error
-// 				if(err.code === 11000) {
-// 					const msg = {
-// 						status: 'failed',
-// 						source: 'REST',
-// 						action: 'Add - Tree',
-// 						message: 'Tree ID already exists'
-// 					};
-// 					res.json(msg);
-// 				} else {
-// 					const msg = {
-// 						status: 'failed',
-// 						source: 'REST',
-// 						action: 'Add - Tree',
-// 						message: err
-// 					};
-// 				}
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Add - Tree',
-// 					message: tree
-// 				});
-// 			}
-// 		});
-// 	});
-//
-// router.route('/trees/:treeId')
-//
-// 	// Gets specified tree
-// 	.get((req, res) => {
-// 		Tree.findById(req.params.treeId, (err, tree) => {
-// 			if(err) {
-// 				res.send(err);
-// 			} else {
-// 				res.json(tree);
-// 			}
-// 		});
-// 	})
-//
-// 	// Edits a specified tree
-// 	.put((req, res) => {
-// 		/*
-// 			Example req.body
-// 			{
-// 				description: 'change me'
-// 			}
-// 		*/
-// 		const id = req.params.treeId;
-// 		const update = { $set: { 'description': req.body.description } };
-// 		const options = { new: true };
-// 		Tree.findByIdAndUpdate(id, update, options, (err, doc) => {
-// 			if(err) {
-// 				res.json({
-// 					status: 'failed',
-// 					source: 'REST',
-// 					action: 'Edit - Tree',
-// 					message: err
-// 				});
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Edit - Tree',
-// 					message: doc
-// 				});
-// 			}
-// 		});
-// 	})
-//
-// 	// Deletes specified tree
-// 	.delete((req, res) => {
-// 		Tree.findByIdAndDelete(req.params.treeId, (err, tree) => {
-// 			if(err) {
-// 				res.json({
-// 					status: 'failed',
-// 					source: 'REST',
-// 					action: 'Delete - Tree',
-// 					message: err
-// 				});
-// 			} else {
-// 				res.json({
-// 					status: 'success',
-// 					source: 'REST',
-// 					action: 'Delete - Tree',
-// 					message: tree
-// 				});
-// 			}
-// 		});
-// 	});
+const host = 'sgu.pdm-commsult.intranet'
+const dbUser = 'root'
+const dbPass = '!QAZxsw2'
+const dbName = 'employees'
+
+router.route('/employees')
+	.get(async(req, res)=>{
+		const conn = db(host, dbUser, dbPass, dbName )
+
+		conn.connect()
+		if (typeof req.query.limit!=='undefined') {
+			res.json(await getEmployees(conn, req.query.limit))
+		}else {
+			res.json(await getEmployees(conn))
+		}
+		conn.end()
+	})
+
+	.post(async(req, res)=>{
+		const conn = db(host, dbUser, dbPass, dbName )
+		conn.connect()
+		if (await insertEmployee(conn, req.body.emp_no, req.body.first_name, req.body.last_name, req.body.birth_date, req.body.gender, req.body.title, req.body.hire_date, req.body.dept_name)===true){
+			res.json({status: 'success'})
+		} else{
+			res.json({status: 'failed'})
+		}
+		conn.end()
+	})
+
+	.put(async(req, res)=>{
+		const conn = db(host, dbUser, dbPass, dbName )
+		conn.connect()
+		if (await updateEmployee(conn, req.body.emp_no, req.body.first_name, req.body.last_name, req.body.birth_date, req.body.gender, req.body.title, req.body.hire_date, req.body.dept_name)===true){
+			res.json({status: 'success'})
+		} else{
+			res.json({status: 'failed'})
+		}
+		conn.end()
+	})
+
+	.delete(async(req, res)=>{
+		const conn = db(host, dbUser, dbPass, dbName )
+		conn.connect()
+		if (await deleteEmployee(conn, req.query.emp_no)===true){
+			res.json({status: 'success'})
+		} else{
+			res.json({status: 'failed'})
+		}
+		conn.end()
+	})
 
 export default router;
